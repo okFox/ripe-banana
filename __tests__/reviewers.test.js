0b6fe1d6c207f86ba2b254eb01fa35b32b693e01
okFox <smayfield@gmail.com>
2020-01-06 15:14:58 -0800

    reviewer model and routes tests passing

---
+++ __tests__/reviewers.test.js
@@ -0,0 +1,110 @@
+
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Reviewer = require('../lib/models/Reviewer');
+
+
+describe('reviewer routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  // beforeEach(() => {
+  //   return mongoose.connection.dropDatabase();
+  // });
+
+
+  let reviewer;
+  let reviewers = [];
+
+  beforeEach(async() => {
+    reviewer = await Reviewer.create({
+      name: 'Bob Phonic',
+      company: 'Amazon Verified Customer'
+    });
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can create a new reviewer', () => {
+    return request(app)
+      .post('/api/v1/reviewers')
+      .send({
+        name: 'Bob Phonic',
+        company: 'Amazon Verified Customer'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Bob Phonic',
+          company: 'Amazon Verified Customer',
+          __v: 0
+        });
+      });
+  });
+
+  it('gets an reviewer by id', () => {
+    return request(app)
+      .get(`/api/v1/reviewers/${reviewer.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Bob Phonic',
+          company: 'Amazon Verified Customer',
+          __v: 0
+        });
+      });
+
+  });
+
+
+
+  it('get all reviewers', async() => {
+
+    return request(app)
+      .get('/api/v1/reviewers')
+      .then(res => {
+        reviewers.forEach(reviewer => {
+          expect(res.body).toContain({
+            _id: reviewer._id.toString(),
+            name: reviewer.name,
+          });
+        });
+      });
+  });
+
+
+  it('updates an reviewer', () => {
+    return request(app)
+      .patch(`/api/v1/reviewers/${reviewer.id}`)
+      .send({ name: 'Corey' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Corey',
+          company: 'Amazon Verified Customer',
+          __v: 0
+        });
+      });
+  });
+
+  it('deletes an reviewer', () => {
+    return request(app)
+      .delete(`/api/v1/reviewers/${reviewer.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Bob Phonic',
+          company: 'Amazon Verified Customer',
+          __v: 0
+        });
+      });
+  });
+});
+
---
+++ routes/reviewers.js
@@ -0,0 +1,54 @@
+
+const { Router } = require('express');
+const Reviewer = require('../lib/models/Reviewer');
+
+
+module.exports = Router()
+  .post('/', (req, res, next) => {
+    Reviewer
+      .create(req.body)
+      .then(reviewer => res.send(reviewer))
+      .catch(next);
+  })
+
+//get all with pagination
+  .get('/', (req, res, next) => {
+
+    const { page = 1, perPage = 25 } = req.query;
+    Reviewer
+      .find()
+      .limit(Number(perPage))
+      .skip((Number(page) - 1) * Number(perPage))
+      .then(reviewers => res.send(reviewers))
+      .catch(next);
+  })
+
+  .get('/search', (req, res, next) => {
+    Reviewer
+      .getAuthorWithPartialText(req.query.partialText)
+      .then(reviewer => res.send(reviewer))
+      .catch(next);
+  })
+
+
+  .get('/:id', (req, res, next) => {
+    Reviewer
+      .findById(req.params.id)
+      .then(Reviewer => res.send(Reviewer))
+      .catch(next);
+  })
+
+  .patch('/:id', (req, res, next) => {
+    Reviewer
+      .findByIdAndUpdate(req.params.id, req.body, { new: true })
+      .then(reviewer => res.send(reviewer))
+      .catch(next);
+  })
+
+  .delete('/:id', (req, res, next) => {
+    Reviewer
+      .findByIdAndDelete(req.params.id)
+      .then(Reviewer => res.send(Reviewer))
+      .catch(next);
+  });
+  
