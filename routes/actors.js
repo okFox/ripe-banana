f9c3e48a83a171923ea9d54945caf2c2088e1bb9
okFox <smayfield@gmail.com>
2020-01-07 17:27:42 -0800

    all special routes except film ID working

--- routes/actors.js
+++ routes/actors.js
@@ -24,31 +24,12 @@ module.exports = Router()
       .catch(next);
   })
 
-//   .get('/search', (req, res, next) => {
-//     Actor
-//       .getAuthorWithPartialText(req.query.partialText)
-//       .then(actor => res.send(actor))
-//       .catch(next);
-//   })
-
 
   .get('/:id', (req, res, next) => {
     Actor
       .findById(req.params.id)
+      .then(actors => actors.addFilms())
       .then(Actor => res.send(Actor))
       .catch(next);
-  })
-
-  // .patch('/:id', (req, res, next) => {
-  //   Actor
-  //     .findByIdAndUpdate(req.params.id, req.body, { new: true })
-  //     .then(actor => res.send(actor))
-  //     .catch(next);
-  // })
-
-  .delete('/:id', (req, res, next) => {
-    Actor
-      .findByIdAndDelete(req.params.id)
-      .then(actor => res.send(actor))
-      .catch(next);
   });
+
